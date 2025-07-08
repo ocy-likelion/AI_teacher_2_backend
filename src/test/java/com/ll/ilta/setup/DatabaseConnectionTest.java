4bd2e872fa6bb29065af354f3fed318abaa9dc3c
yeonjuLee <yeonju509@gmail.com>
2025-07-08 23:07:43 +0900

    PostgreSQL과 스프링부트 연동 성공

---
+++ src/test/java/com/ll/ilta/setup/DatabaseConnectionTest.java
@@ -0,0 +1,31 @@
+package com.ll.ilta.setup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class DatabaseConnectionTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @DisplayName("DB 연결 테스트")
+    @Test
+    void testDatabaseConnection() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            assertThat(connection).isNotNull();
+
+            String dbName = connection.getMetaData().getDatabaseProductName();
+            String dbVersion = connection.getMetaData().getDatabaseProductVersion();
+            System.out.println("DB 연결 성공: " + dbName + " " + dbVersion);
+        }
+    }
+}
