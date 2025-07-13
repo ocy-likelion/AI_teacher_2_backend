d8c610b74b40af27e2102c1f5457d98ff8f137ef
suuny0321 <joosun7647@naver.com>
2025-07-13 14:07:22 +0900

    줄바꿈 주석처리 제거

--- src/main/java/com/ll/ilta/domain/member/v1/controller/MemberV1Controller.java
+++ src/main/java/com/ll/ilta/domain/member/v1/controller/MemberV1Controller.java
@@ -1,5 +1,7 @@
 package com.ll.ilta.domain.member.v1.controller;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import com.ll.ilta.domain.member.v1.dto.MemberLoginRequestDto;
 import com.ll.ilta.domain.member.v1.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,12 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE)
 @Tag(name = "MemberV1Controller", description = " 로그인  API")
-
 public class MemberV1Controller {
 
     private final MemberService memberService;
@@ -25,7 +25,6 @@ public class MemberV1Controller {
 
     @PostMapping("/login")
     @Operation(summary = "로그인 ")
-
     public ResponseEntity<String> login(@RequestBody MemberLoginRequestDto dto) {
         boolean isSuccess = memberService.login(dto);
 
--- src/main/java/com/ll/ilta/domain/member/v1/entity/Member.java
+++ src/main/java/com/ll/ilta/domain/member/v1/entity/Member.java
@@ -14,7 +14,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-
 public class Member {
 
     @Id
