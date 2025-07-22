fdc650b2222f5dde5fb403ae4233d18e73cb5d83
yeonjuLee <yeonju509@gmail.com>
2025-07-23 04:07:15 +0900

    Swagger-Ui 추가

--- src/main/java/com/ll/ilta/domain/image/controller/ImageV1Controller.java
+++ src/main/java/com/ll/ilta/domain/image/controller/ImageV1Controller.java
@@ -2,6 +2,8 @@ package com.ll.ilta.domain.image.controller;
 
 import com.ll.ilta.domain.problem.dto.ProblemResponseDto;
 import com.ll.ilta.domain.problem.service.ProblemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "ImageV1Controller", description = "이미지 업로드 및 문제/해설 생성 API")
 @RequestMapping("/api/v1/image")
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +20,7 @@ public class ImageV1Controller {
 
     private final ProblemService problemService;
 
+    @Operation(summary = "업로드 및 저장", description = "이미지 업로드 및 문제/해설 생성")
     @PostMapping("/upload")
     public ResponseEntity<ProblemResponseDto> uploadImage(@RequestParam("file") MultipartFile file) {
         Long userId = 8L; // TODO: 인증 구현 후 실제 로그인한 유저 ID로 변경
