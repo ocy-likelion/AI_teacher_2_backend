2f940eef7aedec39be4990067b1bd13c6e6b5f94
yeonjuLee <yeonju509@gmail.com>
2025-07-22 14:25:08 +0900

    컨벤션에 의한 Image 엔티티 수정

--- src/main/java/com/ll/ilta/domain/image/entity/Image.java
+++ src/main/java/com/ll/ilta/domain/image/entity/Image.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,13 +26,12 @@ public class Image {
     @JoinColumn(name = "problem_id", nullable = false, unique = true)
     private Problem problem;
 
-    @Builder
-    private Image(String imageUrl, Problem problem) {
+    public Image(String imageUrl, Problem problem) {
         this.imageUrl = imageUrl;
         this.problem = problem;
     }
 
     public static Image of(String imageUrl, Problem problem) {
-        return Image.builder().imageUrl(imageUrl).problem(problem).build();
+        return new Image(imageUrl, problem);
     }
 }
