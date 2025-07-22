6a877b38a46ee375a71bef9104b1260fd1bccac9
yeonjuLee <yeonju509@gmail.com>
2025-07-22 10:15:37 +0900

    배포 dev버전도 security config 추가

--- src/main/java/com/ll/ilta/global/config/SecurityConfig.java
+++ src/main/java/com/ll/ilta/global/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://ilta.onrender.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://ilta.onrender.com", "https://ilta-dev.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
