0afcbd58606a0e03602a54fbb1401fdb24010f17
suuny0321 <joosun7647@naver.com>
2025-07-23 17:53:42 +0900

    응답 통일 및 에러 핸들러 설정 추가 - 응답 통일 - 에러 핸들러

---
+++ src/main/java/com/ll/ilta/global/TestController.java
@@ -0,0 +1,24 @@
+package com.ll.ilta.global;
+
+import com.ll.ilta.global.payload.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final TestService testService;
+
+    @GetMapping("/")
+    public BaseResponse<String> test() {
+        return BaseResponse.onSuccess("성공!");
+    }
+
+    @GetMapping("/failed")
+    public BaseResponse<String> failedTest() {
+        testService.failedTest();
+        return BaseResponse.onSuccess("성공!");
+    }
+}
