6fb50b22d9e3588d8dbbb7ed1cf6391c5181ec89
ysang989 <67964576+ysang989@users.noreply.github.com>
2025-07-24 11:22:48 +0900

    🐛Fix : result 값, concept id 나오게 수정

--- src/main/java/com/ll/ilta/domain/problem/dto/ConceptDto.java
+++ src/main/java/com/ll/ilta/domain/problem/dto/ConceptDto.java
@@ -6,19 +6,21 @@ import lombok.Getter;
 @Getter
 public class ConceptDto {
 
+    private final Long id;
     private final String name;
     private final String description;
 
-    private ConceptDto(String name, String description) {
+    private ConceptDto(Long id, String name, String description) {
+        this.id = id;
         this.name = name;
         this.description = description;
     }
 
-    public static ConceptDto of(String name, String description) {
-        return new ConceptDto(name, description);
+    public static ConceptDto of(Long id, String name, String description) {
+        return new ConceptDto(id, name, description);
     }
 
     public Concept toEntity() {
-        return Concept.of(name, description);
+        return Concept.of(id, name, description);
     }
 }
